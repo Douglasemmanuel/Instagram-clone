ec4eb04047965ef9e188ad5affa9103d57dc6da5
DOUGLAS EMMANUEL <emmanueldouglas2121@gmail.com>
2023-07-24 10:58:49 +0100

    started mty project

---
+++ components/Headers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Headers() {
+  return (
+    <div>
+       <div>
+       <h1 className="cursor-pointer">Instagram</h1>
+       </div>
+       <div>
+        <p>Right Sides</p>
+       </div>
+    </div>
+  )
+}
+
+export default Headers
